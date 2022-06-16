637b1bcd31cbfb2e4dea2f89c4cc4923a3836335
mahya <m.kabiri@kian.digital>
2022-06-16 20:03:15 +0430

    create header title

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -1,5 +1,19 @@
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import { useRouter } from "next/router";
+import styles from "./header.module.scss";
+import { HEADER_TITLE } from "./headerTitle.data";
 export default function Header() {
-	return <div>سلااااااااااااام</div>;
+	const [title, setTitle] = useState<string>();
+	const router = useRouter();
+	useEffect(()=>{
+		setTitle(router.pathname.slice(1, router.pathname.length))
+	},[router.pathname])
+	return (
+		<div className={styles.headerContainer}>
+			<div className={styles.headerTitle}>
+				{title ? HEADER_TITLE[title] : ''}
+			</div>
+			<div className={styles.headerDetailWrapper}></div>
+		</div>
+	);
 }
---
+++ components/layout/header/headerTitle.data.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+export const HEADER_TITLE: Record<string, string> = {
+    questionDetail: 'جزییات سوال',
+    myQuestions: 'لیست سوالات'
+}

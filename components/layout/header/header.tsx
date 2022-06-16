05cd2edb57653980f32fc6715022741bcab45614
mahya <m.kabiri@kian.digital>
2022-06-16 20:19:55 +0430

    create Button

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -1,19 +1,28 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import styles from "./header.module.scss";
 import { HEADER_TITLE } from "./headerTitle.data";
+import { ButtonWrapper } from "../../widgets";
 export default function Header() {
 	const [title, setTitle] = useState<string>();
 	const router = useRouter();
-	useEffect(()=>{
-		setTitle(router.pathname.slice(1, router.pathname.length))
-	},[router.pathname])
+	useEffect(() => {
+		setTitle(router.pathname.slice(1, router.pathname.length));
+	}, [router.pathname]);
 	return (
 		<div className={styles.headerContainer}>
 			<div className={styles.headerTitle}>
-				{title ? HEADER_TITLE[title] : ''}
+				{title ? HEADER_TITLE[title] : ""}
 			</div>
-			<div className={styles.headerDetailWrapper}></div>
+			<div className={styles.headerDetailWrapper}>
+				<ButtonWrapper onClick={() => console.log("hi")} appearance="primary">
+					+ سوال جدید
+				</ButtonWrapper>
+				<div className={styles.avatar}>
+					<img src="../../../static/images/avatar.png" alt="avatar"/>
+				</div>
+				<div className={styles.userName}></div>
+			</div>
 		</div>
 	);
 }

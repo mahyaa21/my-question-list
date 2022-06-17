2baa1c343ccd377454b6cfdf3a324f79ed92fccf
mahya <m.kabiri@kian.digital>
2022-06-17 12:15:17 +0430

    create new question modal data

--- components/createNewQuestion/createNewQuestion.tsx
+++ components/createNewQuestion/createNewQuestion.tsx
@@ -1,13 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./createNewQuestion.module.scss";
-import { ButtonWrapper } from "../widgets";
+import { ButtonWrapper, InputWrapper, TextAreaWrapper } from "../widgets";
 interface CreateNewQuestionProps {
 	onClose: () => void;
 }
 const CreateNewQuestion = ({ onClose }: CreateNewQuestionProps) => {
+	const [questionTitle, setQuestionTitle] = useState<string>();
+	const [questionDescription, setQuestionDescription] = useState<string>();
 	return (
 		<div className={styles.createNewQuestionContainer}>
 			<div className={styles.createNewQuestionHeader}>ایجاد سوال جدید</div>
+			<div className={styles.createNewQuestionContentWrapper}>
+				<InputWrapper
+					label={"موضوع"}
+					value={questionTitle}
+					onChange={(e) => setQuestionTitle(e.target.value)}
+					className={styles.newQuestionTitle}
+				/>
+				<TextAreaWrapper
+					label="متن سوال"
+					value={questionDescription}
+					onChange={(e) => setQuestionDescription(e.target.value)}
+					minimumRows={8}
+				/>
+			</div>
 			<div className={styles.createNewQuestionFooter}>
 				<ButtonWrapper onClick={onClose} appearance="link">
 					انصراف

2b510b72b9912bbb78e429d9cd6eb0ac90db5f24
mahya <mahya@mahyas-MacBook-Pro.local>
2022-06-18 17:14:06 +0430

    fix next-config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-// const path = require('path')
-// const withSass = require('@zeit/next-sass');
-// module.exports = withSass({
-//   cssModules: true,
-//   cssLoaderOptions: {
-//     localIdentName: '[path]___[local]___[hash:base64:5]',
-//   }
-// })
 const path = require('path')
 const withSass = require('@zeit/next-sass');
 module.exports = withSass({
   cssModules: true
 })
 module.exports = {
-  /* Add Your Scss File Folder Path Here */
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
